849d0994ba299ce8b8f9ca2c863ba406b818156c
mrudhulasunku <90451893+mrudhulasunku@users.noreply.github.com>
2021-09-18 12:41:20 +0530

    README.md

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+<script>
+  var sportName = faker.cricket(); 
+  var Name = faker.DHONI(); // DHONI
+  var Name = faker.ROHITH(); // ROHITH SHARMA
+  var Name = faker.VIRAT (); //VIRAT KOHLI
+  var Name = faker.BUMRA(); // BUMRA
+  var Name = faker.MANISH(); // MANISH PANDEY
+  var Name = faker.HARDIK(); // HARDIK PANDEY
+  var Name = faker.DAVID(); // DAVID
+  var Name = faker.STIVSMITH(); // STIVSMITH 
+</script>
+
+
