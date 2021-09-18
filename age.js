a7a208bfe663bdf38bd68d09bf1338f1e08b5589
mrudhulasunku <90451893+mrudhulasunku@users.noreply.github.com>
2021-09-18 12:38:24 +0530

    commits updated

---
+++ age.js
@@ -0,0 +1,11 @@
+<script>
+var DHONI = faker.age.36(); // Caitlyn Kerluke
+var VIRAT = faker.age.30();
+  var ROHITH = faker.age.32(); // ROHITH SHARMA
+  var BUMRA= faker.age.29(); // BUMRA
+  var MANISH = faker.age.33(); // MANISH PANDEY
+  var HARDIK= faker.age.28(); // HARDIK PANDEY
+  var DAVID = faker.age.40(); // DAVID
+  var STIVSMITH = faker.age.41(); // STIVSMITH 
+
+</script>
---
+++ country.js
@@ -0,0 +1,12 @@
+<script>
+var DHONI = faker.INDIA(); // INDIA
+var VIRAT = faker.INDIA();
+  var ROHITH = faker.INDIA(); // ROHITH SHARMA
+  var BUMRA= faker.INDIA(); // BUMRA
+  var MANISH = faker.INDIA(); // MANISH PANDEY
+  var HARDIK= faker.INDIA(); // HARDIK PANDEY
+  var DAVID = faker.AUSTRALIA(); // DAVID
+  var STIVSMITH = faker.AUSTRALIA(); // STIVSMITH 
+
+
+</script>
